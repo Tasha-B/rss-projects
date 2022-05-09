579aaf6f27e1003522cf498419ca89c7df1b7b28
Natalia Belyatskaya <natbeliatsky@gmail.com>
2022-05-10 00:59:16 +0300

    feat" add keys event

--- keyboard/index.js
+++ keyboard/index.js
@@ -2,9 +2,12 @@
 
 'use strict';
 
+const bodyElement = document.body;
+const childs = bodyElement.childNodes;
 const container = document.createElement('div');
 container.classList.add('container');
 document.body.appendChild(container);
+console.log(childs);
 
 const area = document.createElement('textarea');
 area.classList.add('area');
@@ -20,10 +23,16 @@ area.addEventListener('keydown', e => {
 const keyboard = document.createElement('div');
 keyboard.classList.add('keyboard');
 container.appendChild(keyboard);
+keyboard.addEventListener('keydown', e => {
+  console.log(e);
+  if (e.code === 50) {
+      e.preventDefault();
+  }
+  });
 
 const keyCodes = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Delete',
-'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 
+'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
 'ControlLeft', 'Win', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
 
@@ -52,62 +61,42 @@ const ruKeysShifted = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')',
 'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', '◄', '▼', '►'];
 
 keys.forEach((el) => {
-    const keyBtn = document.createElement('button');
+    let keyBtn = document.createElement('button');
     keyBtn.className = 'btn';
     keyBtn.innerText = el;
+    //keyBtn.setAttribute('data-code', 'keyCodes[]');
     keyboard.appendChild(keyBtn);
-    //keyBtn.dataset.code = `${}`;
-});
-
-keyboard.addEventListener('click', e => {
-    console.log(`This key ${e.code} (${e.key})`);
-});
-
-document.addEventListener('keydown', (event) => {
-    const keyBtn = event.key;
-  
-    if (keyBtn === 'Control') {
-      // not alert when only Control key is pressed.
-      return;
-    }
-  
-    if (event.ctrlKey) {
-      // Хотя event.key это не 'Control' (например, нажата клавиша 'a'),
-      // то всё же event.ctrlKey может быть true, если ударживается клавиша Ctrl.
-      console.log(`Combination of ctrlKey + ${keyBtn}`);
-    } else {
-        console.log(`Key pressed ${keyBtn}`);
-    }
-  }, false);
-  
-  document.addEventListener('keyup', (event) => {
-    const keyBtn = event.key;
-  
-    // Как только пользователь отпустит клавишу Ctrl, то она больше не будет активной.
-    // Поэтому event.ctrlKey = false.
-    if (keyBtn === 'Control') {
-        console.log('Control key was released');
-    }
-  }, false);
-
-  keyboard.addEventListener('click', (e) => {
-    console.log(
-      "Key Pressed: " + String.fromCharCode(e.code) + "\n"
-      + "charCode: " + e.code + "\n"
-      + "SHIFT key pressed: " + e.shiftKey + "\n"
-      + "ALT key pressed: " + e.altKey + "\n"
-    );
   });
-  
-  
+
+//------------------------------------------------------------
+
+document.addEventListener('keydown', (e) => {
+  const keyBtn = e.key;
+  if (keyBtn === 'Control') {
+    return;
+  }
+  if (e.ctrlKey) {
+    console.log(`Combination of ctrlKey + ${e.code}`);
+  } else {
+    console.log(`Key pressed ${e.code} (${e.key})`);
+  }
+}, false);
+
+document.addEventListener('keydown', (e) => {
+  keys = document.querySelectorAll('.key');
+  for (let i = 0; i < keys.length; i += 1) {
+    if (keyboard.shift) {
+      keys[i].textContent = keyboard.keysShifted[i];
+    } else {
+      keys[i].textContent = keyboard.down[i]; 
+    };
+  };
+});
+
 
 // function init() {
 //     let out = '';
-//      // eslint-disable-next-line no-plusplus
-//     for (let i = 0; i < keys.length; i++) {
-//         if (i === 12 || i === 23) {
-//             out += '<div class="clearfix"></div>';
-//         }
+//     for (let i = 0; i < keys.length; i += 1) { 
 //         out += '<div class="btn" data = "' + keys[i] + '" >'
 //         + String.fromCharCode(keys[i]) + '</div>';
 //     }
@@ -116,42 +105,20 @@ document.addEventListener('keydown', (event) => {
 
 // init();
 
-// document.addEventListener('keydown', e => {
-//     console.log(`This press ${e.code} (${e.key})`);
-// });
+document.addEventListener('keydown', e => {
+  if (e.code === 'ShiftLeft' && (e.shiftKey)) {
+  console.log('Stop all');
+  keyBtn.innerText = keysShifted[j];
+  }
+});
 
-// document.addEventListener('keyup', e => {
-//     console.log(`This stop ${e.code} (${e.key})`);
-// });
+document.addEventListener('keydown', e => {
+    if (e.code === 'KeyZ' && (e.ctrlKey || e.metaKey)) {
+    console.log('Stop all');
+    }
+});
 
-// keyboard.addEventListener('click', e => {
-//     console.log(`This ${e.code} (${e.key})`);
-// });
-
-
-
-//if(obj[val]) else(e.toUpperCase)
-
-// document.addEventListener('keydown', e => {
-//     if (e.code === 'KeyZ' && (e.ctrlKey || e.metaKey)) {
-//     console.log('Stop all');
-//     }
-// });
-
-// document.addEventListener('keydown', e => {
-//     console.log(`This ${e.code} (${e.key})`);
-//     console.log(e.repeat);
-// });
-
-    // document.addEventListener('keydown', e => {
-    //     console.log(e.code);
-    //     console.log(e.key);
-    //     document.querySelector('keyboard_key[data=", ' + e.key + ', "]').classList.add('active');
-    // });
-
-// eslint-disable-next-line func-names
-// document.addEventListener('keydown', (e) => {
-//         if (e.code == 'KeyZ' && (e.ctrlKey || e.metaKey)) {
-//             alert('Отменить!');
-//         }
-//     });
+document.addEventListener('keydown', e => {
+     console.log(`This ${e.code} (${e.key})`);
+    console.log(e.repeat);
+});
